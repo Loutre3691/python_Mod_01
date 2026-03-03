d864e9a704dcde490fc6abe7e780c78ff54d0101
flora <flora.dechecchi@gmailcom>
2026-03-03 22:12:51 +0100

    ex0

--- ex0/ft_garden_intro.py
+++ ex0/ft_garden_intro.py
@@ -1,1 +1,11 @@
 def ft_garden_intro() -> None:
+    name = (input("Plant name: "))
+    height = int(input("height plant: "))
+    age = int(input("age plant: "))
+    print("===Welcome to My Garden===")
+    print(f"Plant: {name}\nHeight: {height} cm \nAge: {age} days")
+    print("===End of Program===")
+
+
+if __name__ == "__main__":
+    ft_garden_intro()
